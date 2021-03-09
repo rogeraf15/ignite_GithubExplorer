c07b834485f0297009b39b193032b5e03d4fbca2
Roger Fernandes <roger.afernandes@hotmail.com>
2021-03-09 12:04:55 -0300

    configurando o webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  //onde esta o arquivo principal
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+  },
+  resolve: {
+      //quais arquivais vai ultilizar
+      extensions: ['.js', '.jsx'],
+  },
+  module: {
+      rules: [
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          //integração do babel com webpack
+          use: 'babel-loader'
+        }
+      ]
+  }
+};
