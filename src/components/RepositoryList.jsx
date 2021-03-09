ea187f56f6dc0e696d9de5faff8bc5c5eaccc8c1
Roger Fernandes <roger.afernandes@hotmail.com>
2021-03-09 17:17:46 -0300

    primeiro component react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,42 @@
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                  <strong>Nome do repo</strong>
+                  <p>Descrição do repo</p>
+
+                  <a href="">
+                    acessar repositório
+                  </a>
+                </li>
+                <li>
+                  <strong>Nome do repo</strong>
+                  <p>Descrição do repo</p>
+
+                  <a href="">
+                    acessar repositório
+                  </a>
+                </li>
+                <li>
+                  <strong>Nome do repo</strong>
+                  <p>Descrição do repo</p>
+
+                  <a href="">
+                    acessar repositório
+                  </a>
+                </li>
+                <li>
+                  <strong>Nome do repo</strong>
+                  <p>Descrição do repo</p>
+
+                  <a href="">
+                    acessar repositório
+                  </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
