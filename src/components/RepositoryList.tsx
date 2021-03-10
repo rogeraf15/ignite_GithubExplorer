a8a480fe68ef1551f5f9c7602da7479f5baab579
Roger Fernandes <roger.afernandes@hotmail.com>
2021-03-10 11:22:27 -0300

    usando typrescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,9 +3,15 @@ import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss';
 import { useEffect, useState } from 'react';
 
+interface Repository {
+  name: string,
+  description: string,
+  html_url: string,
+}
+
 
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
   
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -19,7 +25,7 @@ export function RepositoryList(){
         <ul>
           {repositories.map(repository => {
             return(
-              <RepositoryItem key={repository.id} repository={repository}/>
+              <RepositoryItem key={repository.name} repository={repository}/>
             )
           })}
         </ul>
--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,14 @@ module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   //para sourceMap, para debugar melhor
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js'
   },
   resolve: {
       //quais arquivais vai ultilizar
-      extensions: ['.js', '.jsx'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
 
   //para nao precisar ficar dando reload
@@ -37,7 +37,7 @@ module.exports = {
   module: {
       rules: [
         {
-          test: /\.jsx$/,
+          test: /\.(j|t)sx$/,
           exclude: /node_modules/,
           //integração do babel com webpack
           use: {
